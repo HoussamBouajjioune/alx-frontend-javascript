9f6f7e95473990069e44290792d35b4f6624445c
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-05-23 21:15:11 +0100

    Refactor TypeScript project structure; remove old subject classes and implement new CRUD operations with updated interfaces and Webpack configuration

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,52 @@
+// task_3/js/main.ts
+
+/// <reference path="./crud.d.ts" />
+
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
+
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  age: 23,
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
+
+import { Subjects } from './subjects/Cpp';
+import { Java } from './subjects/Java';
+import { React } from './subjects/React';
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10,
+};
+
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
+
