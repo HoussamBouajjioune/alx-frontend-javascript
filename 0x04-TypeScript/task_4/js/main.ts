ba8f4f7e3b13f6d1cd520ed7e7d2f023ccf74075
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-05-23 21:19:08 +0100

    Refactor TypeScript project structure; add new task files with interfaces, functions for summing credits, and Webpack configuration

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,26 +1,3 @@
-// task_3/js/main.ts
-
-/// <reference path="./crud.d.ts" />
-
-import { RowID, RowElement } from './interface';
-import * as CRUD from './crud';
-
-const row: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-};
-
-const newRowID: RowID = CRUD.insertRow(row);
-
-const updatedRow: RowElement = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-  age: 23,
-};
-
-CRUD.updateRow(newRowID, updatedRow);
-CRUD.deleteRow(newRowID);
-
 import { Subjects } from './subjects/Cpp';
 import { Java } from './subjects/Java';
 import { React } from './subjects/React';
@@ -49,4 +26,3 @@ console.log('React');
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
-
---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,32 @@
+// Define interfaces using nominal typing with brand properties
+interface MajorCredits {
+  credits: number;
+  __brand: 'MajorCredits';
+}
+
+interface MinorCredits {
+  credits: number;
+  __brand: 'MinorCredits';
+}
+
+// Function to sum MajorCredits
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MajorCredits',
+  };
+}
+
+// Function to sum MinorCredits
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    __brand: 'MinorCredits',
+  };
+}
