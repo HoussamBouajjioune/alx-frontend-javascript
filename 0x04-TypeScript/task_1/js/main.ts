0e47b69fb62dcbe3bccbac688d9dfaf6b9b53775
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-05-23 19:10:08 +0100

    Add Directors interface extending Teacher and example usage

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -19,3 +19,20 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+
+// Extend Teacher to create Directors
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example object based on the Directors interface
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
