62a5cab229d62ccff1d7714c7badeb9ec7a41abb
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-05-23 18:01:18 +0100

    Add Teacher interface and example usage to main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -45,3 +45,27 @@ studentsList.forEach((student) => {
 
 table.appendChild(tbody);
 document.body.appendChild(table);
+
+// task_1/js/main.ts
+
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Allow any additional attribute
+}
+
+// Example usage
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
+
+
