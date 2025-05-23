6a7dbd8d52ded2ecfeacbabc257d6b17ae4b6a0d
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-05-23 19:18:10 +0100

    Add teachClass function for subject-specific teaching tasks; include tests

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -71,3 +71,20 @@ function executeWork(employee: Director | Teacher): string {
 // Testing
 console.log(executeWork(createEmployee(200)));   // Getting to work
 console.log(executeWork(createEmployee(1000)));  // Getting to director tasks
+
+
+// Literal type for allowed subjects
+type Subjects = "Math" | "History";
+
+// Function to return teaching task
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else {
+    return "Teaching History";
+  }
+}
+
+// Testing
+console.log(teachClass("Math"));     // Teaching Math
+console.log(teachClass("History"));  // Teaching History
